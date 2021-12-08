5644b907ef616f3075ce848a61ba63bdef539fbd
yuul2 <77230028+yuul2@users.noreply.github.com>
2021-12-08 15:35:05 +0900

    menucomlite

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.navi_box>ul>li>a').mouseover(function(){
+        $('.navi_box_bg').stop().slideDown(300);
+        $('.submenu_box').stop().slideDown(300);
+        });
+        $('.submenu_box').mouseleave(function(){
+            $('.navi_box_bg').stop().slideUp(300);
+            $('.submenu_box').stop().slideUp(300);
+            });   
+})
