f2a00ade37452fc58a15963b63ee7df5c5b11706
yuul2 <77230028+yuul2@users.noreply.github.com>
2021-12-19 18:30:48 +0900

    wow많이 햇다

--- js/main.js
+++ js/main.js
@@ -7,5 +7,50 @@ $(function(){
         $('.submenu_box').mouseleave(function(){
             $('.navi_box_bg').stop().slideUp(300);
             $('.submenu_box').stop().slideUp(300);
-            });   
+            }); 
+            
+            
+        // 네비게이션 end
+
+     $('.bio-si').click(function(){
+         $('div.bio-similar').css('display','block');
+         $('div.pharmaceuticals').css('display','none');
+         $('div.chemical').css('display','none');
+     });
+
+     $('.new-bio').click(function(){
+        $('div.pharmaceuticals').css('display','block');
+        $('div.bio-similar').css('display','none');
+        $('div.chemical').css('display','none');
+    });
+
+    $('.chemi-gene').click(function(){
+        $('div.chemical').css('display','block');
+        $('div.bio-similar').css('display','none');
+        $('div.pharmaceuticals').css('display','none');
+    });
+
+$('.bio-si').on('click',function(){
+    $('ul.research>li:first-child').addClass('active');
+    $('ul.research>li:last-child').removeClass('active');
+    $('ul.research>li:nth-child(2)').removeClass('active')
+
+});
+$('.new-bio').on('click',function(){
+    $('ul.research>li:first-child').removeClass('active');
+    $('ul.research>li:last-child').removeClass('active');
+    $('ul.research>li:nth-child(2)').addClass('active')
+
+});
+$('.chemi-gene').on('click',function(){
+    $('ul.research>li:first-child').removeClass('active');
+    $('ul.research>li:last-child').addClass('active');
+    $('ul.research>li:nth-child(2)').removeClass('active')
+
+});
+   
+        
+        
+        
+       
 });
